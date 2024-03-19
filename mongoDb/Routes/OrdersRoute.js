ae2df43bce5768544b85569eb55fab1f78bcd138
Hari-Dinesh <dinesh@tericsoft.com>
2024-03-19 21:42:41 +0530

    err_corrected

--- mongoDb/Routes/OrdersRoute.js
+++ mongoDb/Routes/OrdersRoute.js
@@ -1,7 +1,5 @@
-const {dashboardData,Order,userDashBoard,dateDashboard}=require('../controllers/OrderController')
+const {dashboardData,Order}=require('../controllers/OrderController')
 const router=require('express').Router()
 router.post('/OrderDetails',Order)
 router.post('/OrderDashboard',dashboardData)
-router.post('/Userdash',userDashBoard)
-router.post('/datedash',dateDashboard)
 module.exports=router
--- mongoDb/controllers/OrderController.js
+++ mongoDb/controllers/OrderController.js
@@ -1,7 +1,8 @@
-const Order = require("../Models/orderModel");
-var validateDate = require("validate-date");//
+let Order = require("../Models/orderModel");
+var validateDate = require("validate-date"); //
+let { isDate } = require('validator');
 module.exports.Order = async (req, res) => {
-  const { data } = req.body;
+  let { data } = req.body;
   try {
     if (!req.body.Phone) {
       return res
@@ -34,180 +35,249 @@ module.exports.Order = async (req, res) => {
 //
 module.exports.dashboardData = async (req, res) => {
   try {
-    let data = await Order.find();
-    const date = new Date();
-    console.log(date);
-  
-    const totalOrderPrice = await Order.aggregate([
+    const userPhoneNumber = req.body.Phone;
+    const currentDate = new Date();
+    const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+    const fromDate = req.body.fromDate?new Date(req.body.fromDate):"";
+    const toDate = req.body.toDate?new Date(req.body.toDate):"";
+
+    let totalOrderPrice = [];
+    let thisMonthStats = [];
+    let thisWeekStats = [];
+
+    if (userPhoneNumber && fromDate && toDate) {
+      console.log(fromDate)
+      console.log(userPhoneNumber,"in if")
+      totalOrderPrice = await Order.aggregate([
+        {
+          $match: {
+            Phone: userPhoneNumber,
+            orderDate: {
+              $gte: fromDate,
+              $lt: toDate,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            averageOrderPrice: { $avg: "$totalPrice" },
+            totalRevenue: { $sum: "$totalPrice" },
+          },
+        },
+      ]);
+
+      thisMonthStats = await Order.aggregate([
+        {
+          $match: {
+            Phone: userPhoneNumber,
+            orderDate: {
+              $gte: firstDayOfMonth,
+              $lt: lastDayOfMonth,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisMonthRevenue: { $sum: "$totalPrice" },
+            monthOrders: { $sum: 1 },
+            ItemdOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+
+      thisWeekStats = await Order.aggregate([
+        {
+          $match: {
+            Phone: userPhoneNumber,
+            orderDate: {
+              $gte: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (currentDate.getDay() - 1)),
+              $lt: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + (7 - currentDate.getDay())),
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisWeekRevenue: { $sum: "$totalPrice" },
+            weekNumberOfOrders: { $sum: 1 },
+            numberOfItemsOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+    } else if (userPhoneNumber) {
+      console.log(userPhoneNumber,"in el1")
+      totalOrderPrice = await Order.aggregate([   
+      {
+        $match: {
+          Phone: userPhoneNumber,
+        },
+      },
       {
         $group: {
           _id: null,
           averageOrderPrice: { $avg: "$totalPrice" },
-          totalRevenue: { $sum: "$totalPrice" }
+          totalRevenue: { $sum: "$totalPrice" },
         },
       },
     ]);
 
-    const thisMonthStats = await Order.aggregate([
-      {
-        $match: {
-          orderDate: {
-            $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
-            $lt: new Date(
-              new Date().getFullYear(),
-              new Date().getMonth() + 1,
-              0
-            ),
+      thisMonthStats = await Order.aggregate([
+        {
+          $match: {
+            Phone: userPhoneNumber,
+            orderDate: {
+              $gte: firstDayOfMonth,
+              $lt: lastDayOfMonth,
+            },
           },
         },
-      },
-      {
-        $group: {
-          _id: null,
-          thisMonthRevenue: { $sum: "$totalPrice" },
-          monthNumberOfOrders: { $sum: 1 },
-          numberOfItemsOrderMonth: { $sum: { $size: "$orderdata" } },
-        },
-      },
-    ]);
-
-    const thisWeekStats = await Order.aggregate([
-      {
-        $match: {
-          orderDate: {
-            $gte: new Date(
-              new Date().getFullYear(),
-              new Date().getMonth(),
-              new Date().getDate() - (new Date().getDay() - 1)
-            ),
-            $lt: new Date(
-              new Date().getFullYear(),
-              new Date().getMonth(),
-              new Date().getDate() + (7 - new Date().getDay())
-            ),
+        {
+          $group: {
+            _id: null,
+            thisMonthRevenue: { $sum: "$totalPrice" },
+            monthOrders: { $sum: 1 },
+            ItemdOrderWeek: { $sum: { $size: "$orderdata" } },
           },
         },
-      },
-      {
-        $group: {
-          _id: null,
-          thisWeekRevenue: { $sum: "$totalPrice" },
-          weekNumberOfOrders: { $sum: 1 },
-          numberOfItemsOrderWeek: { $sum: { $size: "$orderdata" } },
-        },
-      },
-    ]);
+      ]);
 
-    console.log("This Month Stats:", thisMonthStats);
-    console.log("This Week Stats:", thisWeekStats);
+      thisWeekStats = await Order.aggregate([
+        {
+          $match: {
+            Phone: userPhoneNumber,
+            orderDate: {
+              $gte: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (currentDate.getDay() - 1)),
+              $lt: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + (7 - currentDate.getDay())),
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisWeekRevenue: { $sum: "$totalPrice" },
+            weekNumberOfOrders: { $sum: 1 },
+            numberOfItemsOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+    } else if (fromDate && toDate) {
+      totalOrderPrice = await Order.aggregate([
+        {
+          $match: {
+            orderDate: {
+              $gte: fromDate,
+              $lt: toDate,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            averageOrderPrice: { $avg: "$totalPrice" },
+            totalRevenue: { $sum: "$totalPrice" },
+          },
+        },
+      ]);
+
+      thisMonthStats = await Order.aggregate([
+        {
+          $match: {
+            orderDate: {
+              $gte: firstDayOfMonth,
+              $lt: lastDayOfMonth,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisMonthRevenue: { $sum: "$totalPrice" },
+            monthOrders: { $sum: 1 },
+            ItemdOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+
+      thisWeekStats = await Order.aggregate([
+        {
+          $match: {
+            orderDate: {
+              $gte: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (currentDate.getDay() - 1)),
+              $lt: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + (7 - currentDate.getDay())),
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisWeekRevenue: { $sum: "$totalPrice" },
+            weekNumberOfOrders: { $sum: 1 },
+            numberOfItemsOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+    } else {
+      totalOrderPrice = await Order.aggregate([
+        {
+          $group: {
+            _id: null,
+            averageOrderPrice: { $avg: "$totalPrice" },
+            totalRevenue: { $sum: "$totalPrice" },
+          },
+        },
+      ]);
+
+      thisMonthStats = await Order.aggregate([
+        {
+          $match: {
+            orderDate: {
+              $gte: firstDayOfMonth,
+              $lt: lastDayOfMonth,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisMonthRevenue: { $sum: "$totalPrice" },
+            monthOrders: { $sum: 1 },
+            ItemdOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+
+      thisWeekStats = await Order.aggregate([
+        {
+          $match: {
+            orderDate: {
+              $gte: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (currentDate.getDay() - 1)),
+              $lt: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + (7 - currentDate.getDay())),
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            thisWeekRevenue: { $sum: "$totalPrice" },
+            weekNumberOfOrders: { $sum: 1 },
+            numberOfItemsOrderWeek: { $sum: { $size: "$orderdata" } },
+          },
+        },
+      ]);
+    }
+
     const responseData = {
-      totalOrderPrice: totalOrderPrice[0], 
-      thisMonthStats: thisMonthStats[0], 
-      thisWeekStats: thisWeekStats[0] 
+      totalOrderPrice: totalOrderPrice[0],
+      thisMonthStats: thisMonthStats[0],
+      thisWeekStats: thisWeekStats[0],
     };
 
     res.json(responseData);
   } catch (error) {
-    res.send(error);
-  }
-};
-
-module.exports.userDashBoard=async(req,res)=>{
-  const userPhoneNumber=req.body.Phone;
-  const currentDate = new Date();
-const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-console.log(firstDayOfMonth)
-  const thisMonthStatsOfUser = await Order.aggregate([
-    {
-      $match: {
-        Phone: userPhoneNumber,
-        orderDate: {
-          $gte: firstDayOfMonth,
-          $lt: lastDayOfMonth,  
-        },
-      },
-
-    },
-    {
-      $group: {
-        _id: null, 
-        thisMonthRevenue: { $sum: "$totalPrice" },
-        monthOrders:{$sum: 1},
-        ItemdOrderWeek:{ $sum: { $size: "$orderdata" } },//
-      },
-    },
-  ]);
-  const thisWeekStats = await Order.aggregate([
-    {
-      $match: {
-        Phone:userPhoneNumber,//
-        orderDate: {
-          $gte: new Date(
-            new Date().getFullYear(),
-            new Date().getMonth(),
-            new Date().getDate() - (new Date().getDay() - 1)
-          ),
-          $lt: new Date(
-            new Date().getFullYear(),
-            new Date().getMonth(),
-            new Date().getDate() + (7 - new Date().getDay())
-          ),
-        },
-      },
-    },
-    {
-      $group: {
-        _id:null, 
-        thisWeekRevenue: { $sum: "$totalPrice" },
-        weekNumberOfOrders: { $sum: 1 },
-        numberOfItemsOrderWeek: { $sum: { $size: "$orderdata" } },
-      },
-    },
-  ])
-  const userDashDetails={
-    thismonth:thisMonthStatsOfUser[0],
-    thisweek:thisWeekStats[0]
-  }
-  res.json(userDashDetails)
-}
-module.exports.dateDashboard = async (req, res) => {
-  try {
-    if(!validateDate(req.body.fromDate,responseType="boolean")&&!validateDate(req.body.toDate,responseType="boolean")){//
-      return res.send("Enter the valid Date Format")
-    }
-    const fromDate = new Date(req.body.fromDate);
-    const toDate = new Date(req.body.toDate);
-    if (isNaN(fromDate.getTime()) || isNaN(toDate.getTime())) {//checking the date is ?
-      return res.status(400).json({ error: "Invalid date format" });
-    }
-    if(fromDate>toDate){
-      return res.status(400).json({err:"enter valid from date"})//error handled heree....
-    }
-    
-    const dateOutput = await Order.aggregate([
-      {
-        $match: {
-          orderDate: {
-            $gte: fromDate,
-            $lt: toDate,
-          },
-        },
-      },
-      {
-        $group: {//
-          _id: null,
-          Revenue: { $sum: "$totalPrice" },
-          NumberOfOrders: { $sum: 1 },
-          numberOfItemsOrdered: { $sum: { $size: "$orderdata" } },
-        },
-      },
-    ]);
-    const aggregatedData = dateOutput.length > 0 ? dateOutput[0] : {};//
-
-    return res.json(aggregatedData);
-  } catch (error) {
     console.error("An error occurred:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    res.status(500).send("Internal Server Error");
   }
 };
